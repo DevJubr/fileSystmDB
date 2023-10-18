89ae8cfee8f5955e794f76924cd9f72acc1068e1
Jr <jubayerrahman203@gmail.com>
2023-10-18 13:59:17 +0600

    contertin mistex

--- server.js
+++ server.js
@@ -6,15 +6,28 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({ methods: ["PUT", "POST", "GET", "PETCH"], credentials: true }));
-app.use(express.static(path.join(__dirname, "./front-end/dist")));
+// app.use(express.static(path.join(__dirname, "./front-end/dist")));
 
-app.use(express.static(path.join(__dirname, "./front-end/dist")));
+// app.get("*", (req, res) => {
+//   res.sendFile(
+//     path.resolve(__dirname, "./", "front-end", "dist", "index.html")
+//   );
+// });
 
-app.get("/", (req, res) => {
+app.get("*", (req, res, next) => {
   res.sendFile(
-    path.resolve(__dirname, "./", "front-end", "dist", "index.html")
+    path.resolve(__dirname, "./", "front-end", "dist", "index.html"),
+    (err) => {
+      if (err) {
+        next(err);
+      } else {
+        next();
+      }
+    }
   );
 });
+
+app.use(express.static(path.join(__dirname, "./front-end/dist")));
 
 app.listen(3500, () => {
   console.log("listening on 3500");
