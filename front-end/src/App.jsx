084735f5dae74c9a0da4e9107d4d8e81a24e8be4
Jr <jubayerrahman203@gmail.com>
2023-10-18 14:46:06 +0600

    conter lets  gas nabirun tin mistex

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const [values, setvalues] = useState({
+    title: "",
+    text: "",
+  });
+
+  const henelChenge = (e) => {
+    const { name, value } = e.target;
+
+    setvalues((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSub = async (e) => {
+    e.preventDefault();
+
+    // Assuming 'values' is an object containing the data you want to send
+
+    try {
+      const response = await fetch("http://localhost:3500/creatP", {
+        method: "POST", // Use the correct HTTP method (e.g., POST)
+        headers: {
+          "Content-Type": "application/json", // Specify the content type
+        },
+        body: JSON.stringify(values), // Convert 'values' to JSON format
+      });
+
+      if (response.ok) {
+        // Request was successful (status code 200)
+        const responseData = await response.json(); // If the server sends JSON response
+        console.log(responseData);
+      } else {
+        // Handle error responses here
+        console.error("Request failed with status:", response.status);
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
+  // Attach the 'handleSub' function to a form or button's click event as needed
+
+  return (
+    <div>
+      <form onSubmit={handleSub}>
+        <input
+          type="text"
+          placeholder="enter heda"
+          name="title"
+          onChange={henelChenge}
+          value={values.title}
+        />
+        <textarea
+          placeholder="enter heda"
+          name="text"
+          id=""
+          cols="30"
+          rows="10"
+          value={values.text}
+          onChange={henelChenge}
+        ></textarea>
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-
+const fs = require("fs");
 const app = express();
-
+const filePath = "db.json";
 app.use(express.json());
 app.use(cors({ methods: ["PUT", "POST", "GET", "PETCH"], credentials: true }));
 app.use(express.static(path.join(__dirname, "./front-end/dist")));
+
+app.post("/creatP", (req, res) => {
+  // 2. Parse the JSON content
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  const data = JSON.parse(fileContent);
+
+  // 3. Modify the data structure
+  const newItem = req.body; // Assuming you're sending the new item in the request body
+  data.arrayName.push(newItem);
+
+  // 4. Convert the modified data back to JSON
+  const updatedJSON = JSON.stringify(data, null, 2);
+
+  // 5. Write the updated JSON content back to the file
+  fs.writeFileSync(filePath, updatedJSON, "utf8");
+
+  res.status(200).json({ message: "Item added to the array" });
+});
 
 app.get("*", (req, res) => {
   res.sendFile(
