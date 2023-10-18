69da11b4e15701693d0b2c658b900e674973dac1
Jr <jubayerrahman203@gmail.com>
2023-10-18 13:34:34 +0600

    add seerver side code

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ methods: ["PUT", "POST", "GET", "PETCH"], credentials: true }));
+app.use(express.static(path.join(__dirname, "./front-end/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "./", "front-end", "dist", "index.html")
+  );
+});
+
+app.get("/", (req, res) => {
+  res.send("hi bro");
+});
+
+app.listen(3500, () => {
+  console.log("listening on 3500");
+});
+
+module.exports = app;
