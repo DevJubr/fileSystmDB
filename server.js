260c541ce321ebdf2f9f9088aa4fac40216529cc
Jr <jubayerrahman203@gmail.com>
2023-10-18 14:11:28 +0600

    conter lets run tin mistex

--- server.js
+++ server.js
@@ -6,28 +6,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({ methods: ["PUT", "POST", "GET", "PETCH"], credentials: true }));
-// app.use(express.static(path.join(__dirname, "./front-end/dist")));
+app.use(express.static(path.join(__dirname, "./front-end/dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(
-//     path.resolve(__dirname, "./", "front-end", "dist", "index.html")
-//   );
-// });
-
-app.get("*", (req, res, next) => {
+app.get("*", (req, res) => {
   res.sendFile(
-    path.resolve(__dirname, "./", "front-end", "dist", "index.html"),
-    (err) => {
-      if (err) {
-        next(err);
-      } else {
-        next();
-      }
-    }
+    path.resolve(__dirname, "./", "front-end", "dist", "index.html")
   );
 });
-
-app.use(express.static(path.join(__dirname, "./front-end/dist")));
 
 app.listen(3500, () => {
   console.log("listening on 3500");
